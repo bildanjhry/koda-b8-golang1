9e39f5ba7f83b612538e549546c106c3b237083b
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-06 11:31:14 +0700

    feat: add interactive r input into the program

--- main.go
+++ main.go
@@ -11,14 +11,15 @@ func keliling(PI float32, r float32) float32 {
 }
 
 func main() {
-	const r float32 = 12
+	var r float32
 	const PI float32 = 3.14
 	fmt.Println("============================")
 	fmt.Println("LUAS DAN KELILING LINGKARAN")
 	fmt.Printf("============================ \n")
-	fmt.Printf("Diketahui PI = %g, r = %g\n\n", PI, r)
-	fmt.Printf("Keliling = %g \n", keliling(PI, r))
-	fmt.Printf("Luas = %g\n", luas(PI, r))
+	fmt.Print("Masukan jari-jari :")
+	fmt.Scanf("%g", &r)
+	fmt.Printf("\nDiketahui : \nPI = %g\nr = %g\n\n", PI, r)
+	fmt.Printf("Keliling = %.1f \n", keliling(PI, r))
+	fmt.Printf("Luas = %.1f\n", luas(PI, r))
 	fmt.Println("============================")
-
 }
